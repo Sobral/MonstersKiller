6ba4f2c54e637736d23d726ab80cf95a32549fe4
Luciano <luciano.sobral@uds.com.br>
2020-12-13 19:17:04 -0400

    Implementing health bars data structure

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 new Vue({
   el: '#app',
+  computed: {
+    maxHealth() {
+      return `width: ${this.baseHealth * 3}px`;
+    },
+    currentPlayerHealth() {
+      return `width: ${this.playerHealth * 3}px`;
+    },
+    currentMonsterHealth() {
+      return `width: ${this.monsterHealth * 3}px`;
+    }
+  },
   data() {
     return {
       started: false,
+      baseHealth: 100,
+      playerHealth: 100,
+      monsterHealth: 100
     }
   }
 });
